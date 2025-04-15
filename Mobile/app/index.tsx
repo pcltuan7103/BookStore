e0dc94ffb814bb4cb541c454cf2def0f3b0112e8
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-15 22:51:04 +0700

    [SETUP]: Enviroment

---
+++ Mobile/app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={styles.container}
+        >
+            <Text style={styles.title}>123.</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title : {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "red"
+    }
+});
