e9bf26f672b66dfc68669b92188944b0b6814e2a
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-16 13:00:45 +0700

    [DEPLOY]

--- Mobile/store/authStore.ts
+++ Mobile/store/authStore.ts
@@ -10,7 +10,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
         set({ isLoading: true });
 
         try {
-            const response = await fetch("http://localhost:3000/api/auth/register", {
+            const response = await fetch("https://bookstore-2bkc.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
