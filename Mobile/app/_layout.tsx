d5c808264677088aff4f05776908ca008455bef5
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-18 20:21:56 +0700

    [ADD]: Auto Navigation & Tabs Setup

--- Mobile/app/_layout.tsx
+++ Mobile/app/_layout.tsx
@@ -1,18 +1,37 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { useAuthStore } from "@/store/authStore";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+    const router = useRouter();
+    const segments = useSegments();
+
+    const { checkAuth, user, token } = useAuthStore();
+
+    useEffect(() => {
+        checkAuth()
+    }, []);
+
+    useEffect(() => {
+        const isAuthScreen = segments[0] === "auth";
+        const isSignedIn = user && token;
+
+        if (!isAuthScreen && !isSignedIn) router.replace("/auth");
+        else if (isAuthScreen && isSignedIn) router.replace("/tabs");
+    }, [user, token, segments]);
+
     return (
         <SafeAreaProvider>
             <SafeScreen>
                 <Stack screenOptions={{ headerShown: false }} >
-                    <Stack.Screen name="index" />
+                    <Stack.Screen name="tabs" />
                     <Stack.Screen name="auth" />
                 </Stack>
             </SafeScreen>
-            <StatusBar style="dark"/>
+            <StatusBar style="dark" />
         </SafeAreaProvider>
     )
 }
