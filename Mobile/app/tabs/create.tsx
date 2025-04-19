2da59cb403f0cade56c6d80a97012f853f6f39e5
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-19 23:05:46 +0700

    [ADD]: Create Book Screen

--- Mobile/app/tabs/create.tsx
+++ Mobile/app/tabs/create.tsx
@@ -1,10 +1,183 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, TextInput, TouchableOpacity, Image, Alert, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router';
+import styles from '@/assets/styles/create.styles';
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '@/constants/colors';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 export default function Create() {
+    const [title, setTitle] = useState("");
+    const [caption, setCaption] = useState("");
+    const [rating, setRating] = useState(0);
+    const [image, setImage] = useState<string | null>(null);
+    const [imageBase64, setImageBase64] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const router = useRouter();
+
+    const pickImage = async () => {
+        try {
+            //Request permission
+            if (Platform.OS === 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    Alert.alert("Permission Denied", "We need camera roll permissions to upload an image");
+                    return;
+                }
+            }
+
+            //Launch image picker
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: "images",
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 0.5,
+                base64: true,
+            })
+
+            if (!result.canceled) {
+                setImage(result.assets[0].uri);
+
+                if (result.assets[0].base64) {
+                    setImageBase64(result.assets[0].base64);
+                } else {
+                    const base64 = await FileSystem.readAsStringAsync(result.assets[0].uri, {
+                        encoding: FileSystem.EncodingType.Base64,
+                    })
+
+                    setImageBase64(base64);
+                }
+            }
+        } catch (error) {
+            console.error("Error picking image", error);
+            Alert.alert("Error", "There was a problem selecting your image");
+        }
+    }
+
+    const handleSubmit = async () => { }
+
+    const renderRatingPicker = () => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            stars.push(
+                <TouchableOpacity key={i} onPress={() => setRating(i)} style={styles.starButton}>
+                    <Ionicons
+                        name={i <= rating ? "star" : "star-outline"}
+                        size={32}
+                        color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+                    />
+                </TouchableOpacity>
+            )
+        }
+        return <View style={styles.ratingContainer}>{stars}</View>
+    }
+
     return (
-        <View>
-            <Text>Create tab</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <ScrollView
+                contentContainerStyle={styles.container}
+                style={styles.scrollViewStyle}
+            >
+                <View style={styles.card}>
+                    {/* Header */}
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Add Book Recommendation</Text>
+                        <Text style={styles.subtitle}>Share your favorite reads with orthers</Text>
+                    </View>
+                    {/* Form */}
+                    <View style={styles.form}>
+                        {/* Title */}
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Title</Text>
+                            <View style={styles.inputContainer}>
+                                <Ionicons
+                                    name="book-outline"
+                                    size={20}
+                                    color={COLORS.textSecondary}
+                                    style={styles.inputIcon}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter book title"
+                                    placeholderTextColor={COLORS.placeholderText}
+                                    value={title}
+                                    onChangeText={setTitle}
+                                />
+                            </View>
+                        </View>
+
+                        {/* Rating */}
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Your Rating</Text>
+                            {renderRatingPicker()}
+                        </View>
+
+                        {/* Image */}
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Image</Text>
+                            <TouchableOpacity
+                                style={styles.imagePicker}
+                                onPress={pickImage}
+                            >
+                                {
+                                    image ? (
+                                        <Image source={{ uri: image }} style={styles.previewImage} />
+                                    ) : (
+                                        <View style={styles.placeholderContainer}>
+                                            <Ionicons
+                                                name='image-outline'
+                                                size={40}
+                                                color={COLORS.textSecondary}
+                                            />
+                                            <Text style={styles.placeholderText}>Tap to select image</Text>
+                                        </View>
+                                    )
+                                }
+                            </TouchableOpacity>
+                        </View>
+
+                        {/* Caption */}
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Caption</Text>
+                            <TextInput
+                                style={styles.textArea}
+                                placeholder="Write tour review or throughts about this book ..."
+                                placeholderTextColor={COLORS.placeholderText}
+                                value={caption}
+                                onChangeText={setCaption}
+                                multiline
+                            />
+                        </View>
+
+                        {/* Submit */}
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={handleSubmit}
+                            disabled={loading}
+                        >
+                            {loading ? (
+                                <ActivityIndicator color={COLORS.white} />
+                            ) : (
+                                <>
+                                    <Ionicons
+                                        name='cloud-upload-outline'
+                                        size={20}
+                                        color={COLORS.white}
+                                        style={styles.buttonIcon}
+                                    />
+                                    <Text style={styles.buttonText}>Share</Text>
+                                </>
+                            )
+                            }
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
